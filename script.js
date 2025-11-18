39d11c37c9efffe9416b04ea049768dc0b4db582
Faiha-K <kfaihayoonus@gmail.com>
2025-11-18 15:51:52 +0530

    Initial commit of portfolio files

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.project-button').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Project details are not available yet.');
+    });
+});
